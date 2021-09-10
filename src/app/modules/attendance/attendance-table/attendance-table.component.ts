55435563767fde9e0cace1d6df058215c216b573
BlankerGamer <Bankone55@gmail.com>
2021-09-10 14:20:52 +0700

    commit

--- src/app/modules/attendance/attendance-table/attendance-table.component.ts
+++ src/app/modules/attendance/attendance-table/attendance-table.component.ts
@@ -6,6 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attendance-table.component.scss']
 })
 export class AttendanceTableComponent implements OnInit {
+  
+certs:any;
+
+  table: any = {
+    displayedColumns:[
+      "no",
+      "date",
+      "workIn",
+      "workOut",
+      "workTime"
+    ],
+    columns:[
+      {
+        "key":"no",
+        "value":"ลำดับ"
+      },
+      {
+        "key":"date",
+        "value":"วันที่",
+        "controlType":"datetime"
+      },
+      {
+        "key":"workIn",
+        "value":"เข้างาน",
+        "controlType":"datetime"
+      },
+      {
+        "key":"workOut",
+        "value":"ออกงาน",
+        "controlType":"datetime"
+      },
+      {
+        "key":"workTime",
+        "value":"เวลาการทำงาน",
+        "controlType":"datetime"
+      }
+    ],
+    menu:[
+      "edit","download"
+    ]
+  }
 
   constructor() { }
 
